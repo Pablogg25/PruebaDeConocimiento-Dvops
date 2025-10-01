925774394b3ff5d5c2ad50eb74b35a5dabe2a0f7
Pablo <pgonzalezg12@educantabria.es>
2025-10-01 15:13:30 +0200

    Creamos el Proyecto

---
+++ OrganizadorDeTareasEnEquipo/src/main/java/com/mycompany/organizadordetareasenequipo/OrganizadorDeTareasEnEquipo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.organizadordetareasenequipo;
+
+/**
+ *
+ * @author daw2
+ */
+public class OrganizadorDeTareasEnEquipo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
